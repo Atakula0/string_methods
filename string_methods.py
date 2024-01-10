fec8590e8e5e6e27e72e290dee21f77f1978edc0
Atakula0 <154466360+Atakula0@users.noreply.github.com>
2024-01-10 06:04:47 -0500

    Add files via upload

---
+++ string_methods.py
@@ -0,0 +1,12 @@
+variable = "Test test"
+
+#print(len(variable))   #used to find the length of the string
+#print(variable.find("T"))    #used to find specific characters
+#print(variable.capitalize()) #capitalizes the beginning of each word
+#print(variable.upper())    #turns all letters into uppercase characters
+print(variable.lower())     #turns all letters into lowercase characters
+print(variable.isdigit())   #checks to see if your string contains integers
+print(variable.isalpha())   #checks to see if your string only contains letters uninterrupted
+print(variable.count("T"))  #counts the number of occurances of a specific character
+print(variable.replace("t","b")) #used to replace specific characters in a string with different ones
+print(variable*5) #multiplies the string x amount of times
